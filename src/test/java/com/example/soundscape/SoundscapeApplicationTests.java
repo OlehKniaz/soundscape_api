a8ea7d327d826a2f5cdfb498e36f4ac089d8cc94
Oleh Kniaz <oleg.knia@gmail.com>
2023-10-31 20:03:25 +0200

    Init project

---
+++ src/test/java/com/example/soundscape/SoundscapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soundscape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundscapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
